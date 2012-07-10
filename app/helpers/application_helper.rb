4297ccfce6977c425aa8d6ec69343461cafec020
thomas <tomasz_kaleta@mathart.pl>
2012-07-10 22:42:02 +0200

    firs function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+   #funkcja dbajaca o porawny tytul
+   def full_title(page_title)
+     base_title="Ruby to Ruby kurwa tamto"
+     if page_title.empty?
+       base_title
+     else
+       "#{base_title} | #{page_title}"
+     end
+   end
 end
